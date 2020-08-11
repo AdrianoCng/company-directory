ab8e957e5c11f1d88520cf1bf39148ceefda34d6
AdrianoCng <adrianocangiamila@gmail.com>
2020-08-11 08:57:41 +0100

    first commit

--- PHP/config.php
+++ PHP/config.php
@@ -1,10 +1,17 @@
 <?php
 
-	$cd_host = "localhost";
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$cd_host = $url["host"];
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "root";
-	$cd_password = "";
-	$cd_dbname = "companydirectory";
+	$cd_user = $url["user"];
+	$cd_password = $url["pass"];
+	$cd_dbname = substr($url["path"], 1);
 
 ?>
